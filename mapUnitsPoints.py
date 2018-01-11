b649098ad53a84370185b3fc4e9a7da0e3e227b5
FreewheelMaps <freewheel.carto@gmail.com>
2018-01-11 09:30:05 -0500

    mapUnitPoints - used python dict for faster/better
    Instead of join and calculate field, used a python dictionary and data
    access cursors. much faster, much better.

--- mapUnitsPoints.py
+++ mapUnitsPoints.py
@@ -2,14 +2,26 @@
 
 Stations = arcpy.GetParameterAsText(0)
 MapUnits = arcpy.GetParameterAsText(1)
-uniqueID = arcpy.GetParameterAsText(2)
 
 arcpy.env.overwriteOutput = True
 
-arcpy.AddMessage("Adding MapUnits to Stations")
+desc = arcpy.Describe(Stations)
+path = desc.path
 
-arcpy.SpatialJoin_analysis(Stations, MapUnits, "SpatialJoin", "JOIN_ONE_TO_ONE", "KEEP_ALL") 
-arcpy.AddJoin_management(Stations, uniqueID, "SpatialJoin", uniqueID, "KEEP_ALL")
-arcpy.CalculateField_management(Stations, Stations+".MapUnit", "!spatialJoin.MapUnit_1!", "PYTHON_9.3")
-arcpy.RemoveJoin_management(Stations, "SpatialJoin")
-arcpy.Delete_management("SpatialJoin","FeatureClass")
+arcpy.env.workspace = path
+
+arcpy.AddMessage("Adding MapUnits to Point Data")
+sj = arcpy.SpatialJoin_analysis(Stations, MapUnits, "SpatialJoin", "JOIN_ONE_TO_ONE", "KEEP_ALL") 
+
+target = {}
+
+with arcpy.da.SearchCursor(sj,["TARGET_FID","MapUnit_1"]) as cursor:
+    for row in cursor:
+        target[row[0]] = row[1]
+        
+with arcpy.da.UpdateCursor(Stations,["OBJECTID","MapUnit"]) as cursor:
+    for row in cursor:
+        row[1] = target[row[0]]
+        cursor.updateRow(row)
+        
+arcpy.Delete_management(sj)        
